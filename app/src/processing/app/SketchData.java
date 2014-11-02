arduino/Arduino

Make Compiler independent from Sketch.
Create a class SketchData to store all relevant data for a sketch
(trying to keep GUI stuff out of the way).

Moved preprocessing code from Sketch to Compiler.

app/src/processing/app/SketchData.java
@@ -1,0 +1,104 @@
+package processing.app;
+
+import static processing.app.I18n._;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SketchData {
+
+  /**
+   * Name of sketch, which is the name of main file (without .pde or .java
+   * extension)
+   */
+  private String name;
+  
+  private List<SketchCodeDoc> codeDocs = new ArrayList<SketchCodeDoc>();
+
+  private static final Comparator<SketchCodeDoc> CODE_DOCS_COMPARATOR = new Comparator<SketchCodeDoc>() {
+    @Override
+    public int compare(SketchCodeDoc cd1, SketchCodeDoc cd2) {
+      return cd1.getCode().getFileName().compareTo(cd2.getCode().getFileName());
+    }
+  };
+
+
+  public int getCodeCount() {
+    return codeDocs.size();
+  }
+
+  public SketchCodeDoc[] getCodeDocs() {
+    return codeDocs.toArray(new SketchCodeDoc[0]);
+  }
+
+  public void addCodeDoc(SketchCodeDoc sketchCodeDoc) {
+    codeDocs.add(sketchCodeDoc);
+  }
+
+  public void moveCodeDocToFront(SketchCodeDoc codeDoc) {
+    codeDocs.remove(codeDoc);
+    codeDocs.add(0, codeDoc);
+  }
+
+  protected void replaceCode(SketchCode newCode) {
+    for (SketchCodeDoc codeDoc : codeDocs) {
+      if (codeDoc.getCode().getFileName().equals(newCode.getFileName())) {
+        codeDoc.setCode(newCode);
+        return;
+      }
+    }
+  }
+
+  protected void insertCode(SketchCode sketchCode) {
+    addCodeDoc(new SketchCodeDoc(sketchCode, null));
+  }
+
+  protected void sortCode() {
+    Collections.sort(codeDocs, CODE_DOCS_COMPARATOR);
+  }
+
+  public SketchCodeDoc getCodeDoc(int i) {
+    return codeDocs.get(i);
+  }
+
+  public SketchCode getCode(int i) {
+    return codeDocs.get(i).getCode();
+  }
+
+  protected void removeCode(SketchCode which) {
+    for (SketchCodeDoc codeDoc : codeDocs) {
+      if (codeDoc.getCode() == which) {
+        codeDocs.remove(codeDoc);
+        return;
+      }
+    }
+    System.err.println(_("removeCode: internal error.. could not find code"));
+  }
+
+  public int indexOfCodeDoc(SketchCodeDoc codeDoc) {
+    return codeDocs.indexOf(codeDoc);
+  }
+
+  public int indexOfCode(SketchCode who) {
+    for (SketchCodeDoc codeDoc : codeDocs) {
+      if (codeDoc.getCode() == who) {
+        return codeDocs.indexOf(codeDoc);
+      }
+    }
+    return -1;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void clearCodeDocs() {
+    codeDocs.clear();
+  }
+}

solream/EnergiaNG

app/src/processing/app/SketchData.java
@@ -1,0 +1,108 @@
+package processing.app;
+
+import static processing.app.I18n._;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SketchData {
+
+  /**
+   * Name of sketch, which is the name of main file (without .pde or .java
+   * extension)
+   */
+  private String name;
+  
+  private List<SketchCodeDocument> codeDocs = new ArrayList<SketchCodeDocument>();
+
+  private static final Comparator<SketchCodeDocument> CODE_DOCS_COMPARATOR = new Comparator<SketchCodeDocument>() {
+    @Override
+    public int compare(SketchCodeDocument cd1, SketchCodeDocument cd2) {
+      return cd1.getCode().getFileName().compareTo(cd2.getCode().getFileName());
+    }
+  };
+
+
+  public int getCodeCount() {
+    return codeDocs.size();
+  }
+
+  public SketchCodeDocument[] getCodeDocs() {
+    return codeDocs.toArray(new SketchCodeDocument[0]);
+  }
+
+  public void addCodeDoc(SketchCodeDocument sketchCodeDoc) {
+    codeDocs.add(sketchCodeDoc);
+  }
+
+  public void moveCodeDocToFront(SketchCodeDocument codeDoc) {
+    codeDocs.remove(codeDoc);
+    codeDocs.add(0, codeDoc);
+  }
+
+  protected void replaceCode(SketchCode newCode) {
+    for (SketchCodeDocument codeDoc : codeDocs) {
+      if (codeDoc.getCode().getFileName().equals(newCode.getFileName())) {
+        codeDoc.setCode(newCode);
+        return;
+      }
+    }
+  }
+
+  protected void insertCode(SketchCode sketchCode) {
+    addCodeDoc(new SketchCodeDocument(sketchCode, null));
+  }
+
+  protected void sortCode() {
+    if (codeDocs.size() < 2)
+      return;
+    SketchCodeDocument first = codeDocs.remove(0);
+    Collections.sort(codeDocs, CODE_DOCS_COMPARATOR);
+    codeDocs.add(0, first);
+  }
+
+  public SketchCodeDocument getCodeDoc(int i) {
+    return codeDocs.get(i);
+  }
+
+  public SketchCode getCode(int i) {
+    return codeDocs.get(i).getCode();
+  }
+
+  protected void removeCode(SketchCode which) {
+    for (SketchCodeDocument codeDoc : codeDocs) {
+      if (codeDoc.getCode() == which) {
+        codeDocs.remove(codeDoc);
+        return;
+      }
+    }
+    System.err.println(_("removeCode: internal error.. could not find code"));
+  }
+
+  public int indexOfCodeDoc(SketchCodeDocument codeDoc) {
+    return codeDocs.indexOf(codeDoc);
+  }
+
+  public int indexOfCode(SketchCode who) {
+    for (SketchCodeDocument codeDoc : codeDocs) {
+      if (codeDoc.getCode() == who) {
+        return codeDocs.indexOf(codeDoc);
+      }
+    }
+    return -1;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void clearCodeDocs() {
+    codeDocs.clear();
+  }
+}